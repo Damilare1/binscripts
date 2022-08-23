99fcbc3818db7b3de7a05e7b3ec18b456b7b1b8b
Damilare Adedoyin <dadedoyin@wikimedia.org>
2022-08-23 19:49:39 +0100

    update queue to json for mac use

--- queue2jsonpp.sh
+++ queue2jsonpp.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-[ $# -ge 1 -a -f "$1" ] && input="$1" || input="-"
-sed -e 's/\\//g' $input | /usr/bin/python -mjson.tool
+[ $# -ge 1 -a -f "$1" ] && input="$1"
+sed -e 's/\\//g' $input | /usr/local/bin/python3 -mjson.tool
